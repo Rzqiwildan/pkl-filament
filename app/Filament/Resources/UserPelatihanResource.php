ebb35fdebb700bf52b92ac7bf0cf38214681fc7a
Rizqi wildan <126966398+Rzqiwildan@users.noreply.github.com>
2025-05-18 22:47:29 +0700

    deleted edit user_pelatihan resource and transaksi

--- app/Filament/Resources/UserPelatihanResource.php
+++ app/Filament/Resources/UserPelatihanResource.php
@@ -50,7 +50,7 @@ class UserPelatihanResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
